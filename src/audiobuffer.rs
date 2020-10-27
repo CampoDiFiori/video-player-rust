d3fcb7b6f19b022c17bb07ff40cddabf87a5f84e
dudko <dudko@pop-os.localdomain>
2020-10-27 11:37:26 +0100

    Add a soundio git submodule and move audiobuffer to separate file

---
+++ src/audiobuffer.rs
@@ -0,0 +1,57 @@
+use std::convert::TryInto;
+
+pub struct AudioBuffer {
+    lch: Vec<u8>,
+    rch: Vec<u8>,
+    psts: Vec<i64>,
+    read_idx: usize,
+}
+
+const SAMPLE_SIZE: usize = 4;
+
+impl AudioBuffer {
+    pub fn new() -> Self {
+        Self {
+            lch: Vec::new(),
+            rch: Vec::new(),
+            psts: Vec::new(),
+            read_idx: 0,
+        }
+    }
+
+    pub fn frames_remaining(&self) -> usize {
+        (self.lch.len() - self.read_idx) / SAMPLE_SIZE
+    }
+
+    pub fn add_frame_data(&mut self, ffmpeg_frame: &ffmpeg::frame::Audio) {
+        let len = ffmpeg_frame.samples() * SAMPLE_SIZE;
+
+        self.lch.extend_from_slice(&ffmpeg_frame.data(0)[0..len]);
+        self.rch.extend_from_slice(&ffmpeg_frame.data(0)[0..len]);
+        self.psts.push(ffmpeg_frame.pts().unwrap());
+    }
+}
+
+impl Iterator for AudioBuffer {
+    type Item = (f32, f32);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.lch.len() <= self.read_idx + SAMPLE_SIZE {
+            None
+        } else {
+            let lbytes: [u8; SAMPLE_SIZE] = self.lch[self.read_idx..(self.read_idx + SAMPLE_SIZE)]
+                .try_into()
+                .unwrap();
+            let rbytes: [u8; SAMPLE_SIZE] = self.rch[self.read_idx..(self.read_idx + SAMPLE_SIZE)]
+                .try_into()
+                .unwrap();
+
+            let lsample = std::primitive::f32::from_ne_bytes(lbytes);
+            let rsample = std::primitive::f32::from_ne_bytes(rbytes);
+
+            self.read_idx += SAMPLE_SIZE;
+
+            Some((lsample, rsample))
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate ffmpeg_next as ffmpeg;
 extern crate sdl2;
-use std::convert::TryInto;
+
+mod audiobuffer;
+use audiobuffer::AudioBuffer;
 
 #[allow(unused_imports)]
 use ffmpeg::{codec, filter, format, frame, media};
@@ -25,52 +27,6 @@ impl VideoReader {
             video_decoder: None,
             audio_stream_idx: 0,
             video_stream_idx: 0,
-        }
-    }
-}
-
-struct AudioBuffer {
-    lch: Vec<u8>,
-    rch: Vec<u8>,
-    read_idx: usize,
-}
-
-const SAMPLE_SIZE: usize = 4;
-
-impl AudioBuffer {
-    pub fn new() -> Self {
-        Self {
-            lch: Vec::new(),
-            rch: Vec::new(),
-            read_idx: 0,
-        }
-    }
-
-    pub fn frames_remaining(&self) -> usize {
-        (self.lch.len() - self.read_idx) / SAMPLE_SIZE
-    }
-}
-
-impl Iterator for AudioBuffer {
-    type Item = (f32, f32);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.lch.len() <= self.read_idx + 4 {
-            None
-        } else {
-            let lbytes: [u8; SAMPLE_SIZE] = self.lch[self.read_idx..(self.read_idx + SAMPLE_SIZE)]
-                .try_into()
-                .unwrap();
-            let rbytes: [u8; SAMPLE_SIZE] = self.rch[self.read_idx..(self.read_idx + SAMPLE_SIZE)]
-                .try_into()
-                .unwrap();
-
-            let lsample = std::primitive::f32::from_ne_bytes(lbytes);
-            let rsample = std::primitive::f32::from_ne_bytes(rbytes);
-
-            self.read_idx += 4;
-
-            Some((lsample, rsample))
         }
     }
 }
@@ -184,17 +140,7 @@ fn main() -> Result<(), ffmpeg::Error> {
                 audio_decoder.send_packet(&packet)?;
                 audio_decoder.receive_frame(&mut helper_audio_frame)?;
 
-                let len = unsafe {
-                    (*helper_audio_frame.as_ptr()).linesize[0]
-                        / helper_audio_frame.channels() as i32
-                } as usize;
-                audio_buffer
-                    .lch
-                    .extend_from_slice(&helper_audio_frame.data(0)[0..len]);
-                audio_buffer
-                    .rch
-                    .extend_from_slice(&helper_audio_frame.data(0)[0..len]);
-
+                audio_buffer.add_frame_data(&mut helper_audio_frame);
                 ffmpeg_sample_rate = helper_audio_frame.rate() as i32;
                 helper_audio_frame.channel_layout();
             }
